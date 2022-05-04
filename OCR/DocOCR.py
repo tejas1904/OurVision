42f73309bba602f9fd22a95c6e91162299f63568
Tejas M R <totejasmr@gmail.com>
2022-05-04 10:51:46 +0530

    Refactored DocOCR.py
    1. Remove unused import Image from PIL
    2. Removed commented import DocScanner
    3. Removed comments from DocOCR. ocr and fixed formatting
    4. Removed DocScanner code from commented test code and uncommented it.
    5. Removed writing of resized_down image
    6. Put the test code into __main__ module check block

--- OCR/DocOCR.py
+++ OCR/DocOCR.py
@@ -1,43 +1,19 @@
 import cv2
 import pytesseract
-from PIL import Image
-#from DocScanner import DocScanner 
-
-
-
-
 
 class DocOCR:
-    
-    def ocr(self,imagePath=None):
-        
-        image=cv2.imread(imagePath)
-        
-            
-        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def ocr(self, imagePath=None):
+        image = cv2.imread(imagePath)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        #thresh_val, image = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        
-        '''
-        #comment this
-        cv2.imshow("op",image)
-        cv2.waitKey(0) # waits until a key is pressed
-        #till here
-        '''
-        
-        
         return pytesseract.image_to_string(image)
  
-'''
-img = cv2.imread('cell_pic.jpg')
-_,scannedImage = DocScanner.scan(img=img)
-resized_down = cv2.resize(scannedImage, (320,320), interpolation= cv2.INTER_LINEAR)
-cv2.imshow("op",resized_down)
-cv2.waitKey(0) # waits until a key is pressed
 
-cv2.imwrite('output_image.jpg',scannedImage)
-cv2.destroyAllWindows()
-obj=DocOCR()
-stri=obj.ocr(imagePath='output_image.jpg')
-print(stri)
-'''
+if __name__ == "__main__":
+    img = cv2.imread('cell_pic.jpg')
+    resized_down = cv2.resize(img, (320,320), interpolation= cv2.INTER_LINEAR)
+    cv2.imshow("op",resized_down)
+    cv2.waitKey(0) # waits until a key is pressed
+
+    obj=DocOCR()
+    stri=obj.ocr(imagePath='output_image.jpg')
+    print(stri)
