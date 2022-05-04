f510f436b9b998839e1ca33d01ebb3ff3966f497
Tejas M R <totejasmr@gmail.com>
2022-05-04 12:41:11 +0530

    Create paths.py
    Ported all the paths from main

---
+++ paths.py
@@ -0,0 +1,5 @@
+INPUT_IMAGE_PATH="InputImages/camera_image_2.jpeg"
+OUTPUT_IMAGE_PATH="OutputImages/output_camera_image_2.jpeg"
+
+TEXT_DETECTION_MODEL_PATH="Models/frozen_east_text_detection.pb";
+TEXT_RECOGNITION_MODEL_PATH="Models/crnn.onnx";
